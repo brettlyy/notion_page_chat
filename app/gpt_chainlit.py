0984eb1e70053b437a6df52506fc886b576eb071
Brettly <brettcd@icloud.com>
2023-10-25 19:34:11 -0700

    Setup first test of doc search chainlit.

---
+++ app/gpt_chainlit.py
@@ -0,0 +1,118 @@
+import os
+import sys
+from dotenv import load_dotenv
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from langchain.document_loaders import TextLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
+from langchain.llms import openai, HuggingFaceHub
+from langchain.chains import LLMChain, ConversationalRetrievalChain
+from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+
+import chainlit as cl
+
+##########################
+    #Variables
+##########################
+
+load_dotenv()
+#openai_token = os.getenv('OPENAI_API_KEY')
+hf_token = os.getenv('hugging_face_token')
+
+data_dir = './../data/'
+
+##########################
+    #LLM Setup
+##########################
+repo_id = 'google/flan-t5-xxl'
+llm = HuggingFaceHub(repo_id=repo_id, huggingfacehub_api_token=hf_token, model_kwargs={'temperature': 0.25, 'max_length': 1028})
+
+loader = DirectoryLoader(data_dir, glob="*.txt")
+index = VectorstoreIndexCreator().from_loaders([loader])
+
+system_template = """Use the following pieces of context to answer the users question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+ALWAYS return a "SOURCES" part in your answer.
+The "SOURCES" part should be a reference to the source of the document from which you got your answer.
+
+And if the user greets with greetings like Hi, hello, How are you, etc reply accordingly as well.
+
+Example of your response should be:
+
+The answer is foo
+SOURCES: xyz
+
+
+Begin!
+----------------
+{summaries}"""
+messages = [
+    SystemMessagePromptTemplate.from_template(system_template),
+    HumanMessagePromptTemplate.from_template("{question}"),
+]
+prompt = ChatPromptTemplate.from_messages(messages)
+chain_type_kwargs = {"prompt": prompt}
+
+##########################
+    #App Setup
+##########################
+@cl.on_chat_start
+async def on_chat_start():
+    
+    #load documents 
+    loader = DirectoryLoader(data_dir, glob="*.txt")
+    docs = loader.load()[0]
+
+    message_history = ChatMessageHistory()
+
+    memory = ConversationBufferMemory(
+        memory_key="chat_history",
+        output_key="answer",
+        chat_memory=message_history,
+        return_messages=True,
+    )
+
+    # Create a chain that uses the Chroma vector store
+    chain = ConversationalRetrievalChain.from_llm(
+        ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True),
+        chain_type="stuff",
+        retriever=docs.as_retriever(),
+        memory=memory,
+        return_source_documents=True,
+    )
+
+@cl.on_message
+async def main(message: cl.Message):
+    # Your custom logic goes here...
+    chain = cl.user_session.get("chain")  # type: ConversationalRetrievalChain
+    cb = cl.AsyncLangchainCallbackHandler()
+
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res["answer"]
+    source_documents = res["source_documents"]  # type: List[Document]
+
+    text_elements = []  # type: List[cl.Text]
+
+    if source_documents:
+        for source_idx, source_doc in enumerate(source_documents):
+            source_name = f"source_{source_idx}"
+            # Create the text element referenced in the message
+            text_elements.append(
+                cl.Text(content=source_doc.page_content, name=source_name)
+            )
+        source_names = [text_el.name for text_el in text_elements]
+
+        if source_names:
+            answer += f"\nSources: {', '.join(source_names)}"
+        else:
+            answer += "\nNo sources found"
+    # Send a response back to the user
+    await cl.Message(
+        content=f"Received: {message.content}",
+    ).send()
+
